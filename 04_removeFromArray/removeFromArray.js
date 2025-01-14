55391d21e6bc15161f1731801e9a99a9c778dbeb
butler <113191739+SchrodingersMouser@users.noreply.github.com>
2025-01-14 11:10:27 -0600

    complete exercises 4 through 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
 const removeFromArray = function(array, ...excluded) {
-    console.log(array)
+    // console.log(array);
+    // console.log(excluded);
     let newArray =[];
     for( a of array){
-        //console.log(a);
-        while(!(excluded.forEach((element) => return(a === element)))){
+        // console.log(a);
+        if(!excluded.some(element => (a === element))){ //the sum method checks each array element and returns as soon as one is true
             newArray.push(a);
             //console.log(newArray);
         }
@@ -12,7 +13,7 @@ const removeFromArray = function(array, ...excluded) {
     return newArray;
 };
 
-removeFromArray([1,2,3,4],3);
+// removeFromArray([1,2,3,4],3,2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sum = 0
 
+    if(!Number.isInteger(int1) || !Number.isInteger(int2)) { return "ERROR" } //check type is int
+
+    if(int1 <= 0 || int2 <= 0){ return "ERROR"; } //no negative #s
+
+    if(int1 >= int2){ let i = int2; int2 = int1; int1 = i; } //smaller first
+
+    for(let i = int1; i <= int2; i++){
+        sum += i;
+    }
+    console.log(sum);
+    return sum;
 };
+
+sumAll(1,4);
 
 // Do not edit below this line
 module.exports = sumAll;
